406728d2defe213a4679883d93957c939d848913
Sam Peterson <samllama55@gmail.com>
2023-09-19 08:09:40 +0200

    ADD: Partially svleteified component.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,38 @@
 <script lang="ts">
-  import QRCode from 'qrcode';
+	// import QRCode from 'qrcode';
+	// import {QRCode} from 'react-qrcode-logo/dist';
 
+	import QrCode from './QRCode.svelte';
+
+	let url = '';
+  let foo;
+
+	console.log('foo: ', foo);
+
+	// QRCode.(
+	// 	'some text',
+	// 	{
+	// 		errorCorrectionLevel: 'H',
+	// 		color: {
+	// 			// light: '#456',
+	// 			dark: '#ab7003'
+	// 		}
+	// 	},
+	// 	function (err, newUrl) {
+	// 		console.log(newUrl);
+	// 		url = newUrl;
+	// 	}
+	// );
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{@html url}
+
+<!-- <QRCode value="testing12354" /> -->
+<QrCode data={{
+  value: 'https://github.com/gcoro/react-qrcode-logo',
+  size: 500,
+  logoImage: 'https://github.com/gcoro/react-qrcode-logo/blob/master/res/qrcode-ts.png'
+}} />
