70b2a42b36295360dd4884f58421bcd5caba254b
Sam Peterson <samllama55@gmail.com>
2023-09-27 21:12:58 +0200

    CHANGE: JS form instead of  html. This fixes de rendering losing state.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,50 @@
 <script lang="ts">
-	import { QrCode, EcValues, LogoPaddingStyles, QrStyles } from '$lib';
+	import { QrCode, EcValues, LogoPaddingStyles, QrStyles, type QrCodeProps } from '$lib';
 	import { Accordion, AccordionItem, Tab, TabGroup } from '@skeletonlabs/skeleton';
 	// import QrComponent from './QRCode.svelte';
 
 	let qrCode: QrCode;
 
-	// I'm unable to do server side work, so I will use
-	// the submit as if it were a POST to the server.
-	async function handleSubmit(target: EventTarget & HTMLFormElement) {
-		const formData = new FormData(target);
-		const value = QrCode.getFormString(formData.get('value'));
-
-		if (typeof value !== 'string') return;
+	let formValues: QrCodeProps = {
+		// Required
+		value: '',
+		// Common
+		size: 150,
+		// FIXME: Add CSS color regex?
+		bgColor: '',
+		fgColor: '',
 
 		// Image
-		let imageUrl = QrCode.getFormString(formData.get('imageUrl'));
-		const maybeImageFile = formData.get('imageFile');
-		if (maybeImageFile instanceof File && maybeImageFile.size > 0) {
-			imageUrl = await awaitFileLoadToBase64(maybeImageFile);
+		logoImage: '',
+		// Image Details
+		removeQrCodeBehindLogo: false,
+		logoHeight: undefined,
+		logoOpacity: undefined,
+		logoPadding: undefined,
+		logoWidth: undefined,
+
+		// Advanced
+		ecLevel: EcValues.MEDIUM,
+		quietZone: undefined,
+		logoPaddingStyle: LogoPaddingStyles.SQUARE,
+		qrStyle: QrStyles.SQUARES,
+
+		// Enable loading images from external sources...
+		enableCORS: true
+	};
+
+	async function handleSubmit(target: EventTarget & HTMLFormElement) {
+		// Image
+		if (
+			formValues.logoImage !== undefined &&
+			formValues.logoImage.length === 0 &&
+			files !== null &&
+			files.length > 0
+		) {
+			imageUrl = await awaitFileLoadToBase64(files[0]);
 		}
 
-		const tmpQrCode = new QrCode({
-			// Required
-			value,
-			// Common
-			size: QrCode.getFormNumber(formData.get('size')),
-			// FIXME: Add CSS color regex?
-			bgColor: QrCode.getFormString(formData.get('bgColor')),
-			fgColor: QrCode.getFormString(formData.get('fgColor')),
-
-			// Image
-			logoImage: imageUrl,
-			// Image Details
-			removeQrCodeBehindLogo: QrCode.getFormBoolean(formData.get('removeQrCodeBehindLogo')),
-			logoHeight: QrCode.getFormNumber(formData.get('logoHeight')),
-			logoOpacity: QrCode.getFormDouble(formData.get('logoOpacity')),
-			logoPadding: QrCode.getFormNumber(formData.get('imagePadding')),
-			logoWidth: QrCode.getFormNumber(formData.get('logoWidth')),
-
-			// Advanced
-			ecLevel: QrCode.getEcValue(formData.get('ecLevel')),
-			quietZone: QrCode.getFormNumber(formData.get('quietZone')),
-			logoPaddingStyle: QrCode.getLogoPaddingValues(formData.get('logoPaddingStyle')),
-			qrStyle: QrCode.getQrStyleValues(formData.get('qrStyle')),
-
-			// Enable loading images from external sources...
-			enableCORS: true
-		});
+		const tmpQrCode = new QrCode(formValues);
 
 		if (imageUrl) {
 			qrCode = await tmpQrCode.addImage(imageUrl);
@@ -91,6 +89,7 @@
 
 	$: {
 		disableRemove = typeof files === 'undefined' || files === null;
+		console.log(formValues);
 	}
 </script>
 
@@ -107,22 +106,22 @@
 	>
 		<label class="label">
 			<span>QR Value:</span>
-			<input class="input" name="value" type="text" required />
+			<input class="input" name="value" type="text" bind:value={formValues.value} required />
 		</label>
 
 		<label class="label">
 			<span>QR Size:</span>
-			<input class="input" name="size" type="number" step="10" />
+			<input class="input" name="size" type="number" bind:value={formValues.size} step="10" />
 		</label>
 
 		<label class="label">
 			<span>Background color:</span>
-			<input class="input" name="bgColor" type="text" />
+			<input class="input" name="bgColor" bind:value={formValues.bgColor} type="text" />
 		</label>
 
 		<label class="label">
 			<span>Foreground color:</span>
-			<input class="input" name="fgColor" type="text" />
+			<input class="input" name="fgColor" bind:value={formValues.fgColor} type="text" />
 		</label>
 
 		<!-- If they have a link I will recommend the logoPadding -->
@@ -193,7 +192,6 @@
 			</svelte:fragment>
 		</TabGroup>
 
-		<!-- TODO: FIX ACCORDION de-registering with form... -->
 		<Accordion autocollapse>
 			<AccordionItem>
 				<svelte:fragment slot="summary">Image Details</svelte:fragment>
@@ -201,27 +199,55 @@
 					<section class="flex w-full space-x-8">
 						<label class="label md:w-1/2">
 							<span>Adjust Height of Logo:</span>
-							<input class="input" name="logoHeight" type="number" />
+							<input
+								class="input"
+								name="logoHeight"
+								bind:value={formValues.logoHeight}
+								type="number"
+							/>
 						</label>
 
 						<label class="label md:w-1/2">
 							<span>Adjust Width of Logo:</span>
-							<input class="input" name="logoWidth" type="number" />
+							<input
+								class="input"
+								name="logoWidth"
+								bind:value={formValues.logoWidth}
+								type="number"
+							/>
 						</label>
 					</section>
 
 					<label class="label">
 						<span>Image Padding:</span>
-						<input class="input" name="imagePadding" type="number" />
+						<input
+							class="input"
+							name="imagePadding"
+							bind:value={formValues.logoPadding}
+							type="number"
+						/>
 					</label>
 
 					<label class="label">
 						<span>Image Opacity:</span>
-						<input class="input" name="logoOpacity" type="number" min="0" max="1" step="0.1" />
+						<input
+							class="input"
+							name="logoOpacity"
+							bind:value={formValues.logoOpacity}
+							type="number"
+							min="0"
+							max="1"
+							step="0.1"
+						/>
 					</label>
 
 					<label class="flex items-center space-x-2">
-						<input class="checkbox" name="removeQrCodeBehindLogo" type="checkbox" />
+						<input
+							class="checkbox"
+							name="removeQrCodeBehindLogo"
+							bind:checked={formValues.removeQrCodeBehindLogo}
+							type="checkbox"
+						/>
 						<p>Remove QR behind logo:</p>
 					</label>
 				</svelte:fragment>
@@ -232,7 +258,7 @@
 				<svelte:fragment slot="content">
 					<label class="label">
 						Error correction level:
-						<select class="select" name="ecLevel" value={EcValues.MEDIUM}>
+						<select class="select" name="ecLevel" bind:value={formValues.ecLevel}>
 							{#each Object.entries(EcValues) as [label, value]}
 								<option {value}><span>{label.toLowerCase()}</span></option>
 							{/each}
@@ -241,12 +267,12 @@
 
 					<label class="label">
 						<span>Space around QR Code:</span>
-						<input class="input" name="quietZone" type="number" />
+						<input class="input" name="quietZone" bind:value={formValues.quietZone} type="number" />
 					</label>
 
 					<label class="label">
 						Logo Padding Style:
-						<select class="select" name="logoPaddingStyle" value={LogoPaddingStyles.SQUARE}>
+						<select class="select" name="logoPaddingStyle" bind:value={formValues.logoPaddingStyle}>
 							{#each Object.entries(LogoPaddingStyles) as [label, value]}
 								<option {value}>{label.toLowerCase()}</option>
 							{/each}
@@ -255,7 +281,7 @@
 
 					<label class="label">
 						QR Style:
-						<select class="select" name="qrStyle" value={QrStyles.SQUARES}>
+						<select class="select" name="qrStyle" bind:value={formValues.qrStyle}>
 							{#each Object.entries(QrStyles) as [label, value]}
 								<option {value}>{label.toLowerCase()}</option>
 							{/each}
